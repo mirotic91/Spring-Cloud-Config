af81828ca66887ec3edc37a8a5f50a8186b9a352
jonguk <jonguk1221@gmail.com>
2019-06-03 08:43:55 +0900

    refresh config

--- config-client/src/main/java/com/mirotic/configclient/ConfigProperties.java
+++ config-client/src/main/java/com/mirotic/configclient/ConfigProperties.java
@@ -3,9 +3,11 @@ package com.mirotic.configclient;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties("application")
 @Getter @Setter
 public class ConfigProperties {
